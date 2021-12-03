e4648dcca8c7e1756064c0657f83eada657ae772
Mark Allen <markdallen@me.com>
2021-12-03 12:56:47 +0000

    Include child components in home view component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,114 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="l-home home">
+    <div class="l-home__header">
+      <h1 class="l-home__header-title home__header-title">My Blog</h1>
+      <search @search="handleSearch($event)" @clear="load" />
+    </div>
+    <div v-if="!getError">
+      <p
+        class="l-home__search-results home__search-results"
+        v-if="searching"
+      >
+        {{ allPosts.length }} results found
+      </p>
+      <posts :posts="allPosts"/>
+      <pagination
+        v-if="!searching"
+        :position="getPosition"
+        @change-page="handlePageChange($event)"
+      />
+    </div>
+    <p class="l-error error" v-else>
+      There was an error connecting to the server, please try again later.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Posts from '../components/Posts';
+  import Pagination from '../components/Pagination';
+  import Search from '../components/Search';
+  import { mapActions, mapGetters } from 'vuex';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data: () => ({
+      searching: false,
+    }),
+    name: 'Home',
+    computed: {
+      ...mapGetters('posts', [
+        'allPosts',
+        'getPage',
+        'getPosition',
+        'getError',
+      ]),
+    },
+    methods: {
+      ...mapActions('posts', [
+        'getPosts',
+        'nextPage',
+        'prevPage',
+        'searchPosts',
+        'clearError',
+      ]),
+      handlePageChange(direction) {
+        if (direction === 'next') {
+          this.nextPage();
+          this.getPosts();
+        } else {
+          this.prevPage();
+          this.getPosts();
+        }
+      },
+      handleSearch(query) {
+        this.searchPosts(query);
+        this.searching = true;
+      },
+      load() {
+        this.clearError();
+        this.getPosts();
+        this.searching = false;
+      },
+    },
+    mounted() {
+      this.load();
+    },
+    components: {
+      Posts,
+      Pagination,
+      Search,
+    },
+  };
 </script>
+
+<style lang="scss">
+  .l-home__header {
+    margin-bottom: 3em;
+    padding-bottom: 1em;
+    border-bottom: thin solid #eee;
+
+    @media (min-width: 769px) {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      flex-wrap: wrap;
+    }
+
+    &-title {
+      margin: 0;
+    }
+  }
+
+  .home {
+    &__search-results {
+      font-size: 1.25em;
+      font-weight: bold;
+    }
+
+    &__header-title {
+      font-size: clamp(4em, 5em, 5vw);
+      text-align: center;
+      white-space: nowrap;
+    }
+  }
+</style>
